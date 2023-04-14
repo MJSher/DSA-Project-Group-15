3373fdabda45a447bb4f1007fe1d9f84231bfbf6
MJSher <90425926+MJSher@users.noreply.github.com>
2023-04-14 13:45:24 -0500

    Update kmp.h
    Added KMP_Algorithm to header

--- kmp.h
+++ kmp.h
@@ -7,8 +7,10 @@ private:
     std::string pattern;
     std::string text;
     std::vector<int> create_LPS(); //creates LPS string to use for KMP
+    int num_matches; //number of matches
 public:
     KMP(std::string input_text, std::string input_pattern); //so the inputs are CLD's
     void search(int mode); //so I think I will give a mode that asks for things
     void public_LPS(); //public function for testing. Will delete later.
+    void KMP_algorithm();
 };

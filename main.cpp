e2180a607e107629fc365c5fef43388b76c56388
MJSher <90425926+MJSher@users.noreply.github.com>
2023-04-21 09:46:11 -0500

    Update main.cpp
    Changed main to work with the updated return type of KMP_algorithm's function

--- main.cpp
+++ main.cpp
@@ -14,8 +14,16 @@ int main(int argc, char* argv[]) {
     unsigned long c_start, c_end;
     c_start = std::clock();
     thing.public_LPS(); //this is where you put the functions you want to bench mark
+    std::pair<bool, std::vector<int>> pair_var = thing.KMP_algorithm();
+    std::cout << "does it have a match?: " << pair_var.first << ". list of matches if so: " << std::endl;
+    for (int i = 0; i < pair_var.second.size(); i++)
+    {
+        std::cout << pair_var.second[i] << ", ";
+    }
+    std::cout << std::endl;
     c_end = std::clock();
     float output = 1.0 * (c_end - c_start) / CLOCKS_PER_SEC;
+    thing.print_num_matches();
     std::cout << "time: " << output << std::endl;
 
 }

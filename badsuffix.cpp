fd57675470f960287dcfd8bb04aabc15d2a472bb
GunnarGolden <78178407+GunnarGolden@users.noreply.github.com>
2023-04-23 01:16:23 -0400

    Update badsuffix.cpp
    Changed variable names, made code actually functional, added test function (to be removed later).

--- badsuffix.cpp
+++ badsuffix.cpp
@@ -1,35 +1,59 @@
 #include <iostream>
-#define CHARLIM 256
+#include <cmath>
+#include <vector>
+#define CHARLIM 127
 //CHARLIM = character limit
-int biggest(int data1, int data2) {
-   if(data1 > data2)
-   return data1;
-   return data2;
-}
-void BadCharHeur(string string, int size, int badchar[CHARLIM])
+
+void BadCharHeur(std::string string, int size, int badchar[CHARLIM])
 {
-	for (int i = 0; i < CHARLIM; i++)
-		badchar[i] = -1;
-	for (int i = 0; i < size; i++)
-		badchar[(int)string[i]] = i;
+    for (int i = 0; i < CHARLIM; i++) {
+        badchar[i] = -1;
+    }
+    for (int i = 0; i < size; i++) {
+        badchar[(int)string[i]] = i;
+    }
+
 }
-void search(string txt, string pat)
+std::pair<int, std::vector<int>> BMAlgorithim(std::string text, std::string pat)
 {
-	int alength = pat.size();
-	int blength= txt.size();
-	int badchar[CHARLIM];
-	BadCharHeur(pat, blength, badchar);
-	int shift = 0;
-	while(shift <= (blength - alength))
-	{
-		int j = alength - 1;
-		while(j >= 0 && pat[j] == txt[shift + j])
-			j--;
-		if (j < 0)
-		{
-			shift += if(shift + alength < blength) alength-badchar[txt[shift + alength]] else 1;
-		}
-		else
-			shift += max(1, j - badchar[txt[shift + j]]);
-	}
+    int patternLength = pat.size();
+    int textLength= text.size();
+    int badChar[CHARLIM];
+    BadCharHeur(pat, textLength, badChar);
+    int shift = 1;
+    std::vector<int> locations;
+    int found = 0; //used to keep track if at least one match has been found.
+    while(shift <= (textLength - patternLength))
+    {
+        int patternIndex = patternLength - 1;
+        while(patternIndex >= 0 && pat[patternIndex] == text[shift + patternIndex]) { //For as long as the pattern and text match
+            patternIndex--;
+        }
+        if (patternIndex < 0) { //This means the whole pattern matches the text
+            locations.push_back(shift);
+            found = 1;
+            if (shift + patternLength < textLength) {
+                shift += patternLength - badChar[text[shift + patternLength]];
+            } else {
+                shift = 1;
+            }
+        }
+        else { //The patterns does not match the text;
+            shift += std::max(1, patternIndex - badChar[text[shift + patternIndex]]);
+        }
+    }
+    std::pair<int, std::vector<int>> returnValue;
+    returnValue.first = found;
+    returnValue.second = locations;
+    return returnValue;
+}
+
+int main() { //For testing purposes only, to be removed later
+    std::pair<int, std::vector<int>> returnValue;
+    returnValue = BMAlgorithim("Much, much longer stringly. I think ring should appear at least twice, though still somewhat sparingly.", "ring");
+    //In sample text, 'ring' appears at index 20, 36, 96
+    for (int i = 0; i < returnValue.second.size(); i++) {
+        std::cout << returnValue.second[i] << std::endl;
+    }
+    return 0;
 }

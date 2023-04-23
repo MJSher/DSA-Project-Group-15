d8135b38b67a2ce9be7d50fc7ac686e3c9311e5d
andrew56012 <98777690+andrew56012@users.noreply.github.com>
2023-04-23 00:15:03 -0400

    Update badsuffix.cpp

--- badsuffix.cpp
+++ badsuffix.cpp
@@ -22,7 +22,7 @@ void search(string txt, string pat)
 			j--;
 		if (j < 0)
 		{
-			shift += (shift + alength < blength) alength-badchar[txt[shift + alength]] : 1;
+			shift += if((shift + alength < blength)) alength-badchar[txt[shift + alength]] else 1;
 		}
 		else
 			shift += max(1, j - badchar[txt[shift + j]]);

5293deea9dc880842199798d77509446050e1c61
MJSher <90425926+MJSher@users.noreply.github.com>
2023-04-24 21:05:29 -0500

    Update kmp.h
    -Updated the h file so that it is no longer outdated

--- kmp.h
+++ kmp.h
@@ -9,9 +9,10 @@ private:
     std::string text;
     std::vector<int> create_LPS(); //creates LPS string to use for KMP
     int num_matches; //number of matches
+    friend class Find_Replace;
 public:
-    KMP(std::string input_text, std::string input_pattern); //so the inputs are CLA's
-    void public_LPS(); //public function for testing. Will delete later.
-    std::pair<bool, std::vector<int>> KMP_algorithm();
+    KMP(); //so the inputs are CLA's
+    void public_LPS(std::string input_text, std::string input_pattern); //public function for testing. Will delete later.
+    std::pair<bool, std::vector<int>> KMP_algorithm(std::string input_text, std::string input_pattern);
     void print_num_matches();
 };
